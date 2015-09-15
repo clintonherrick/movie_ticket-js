fd36643b2fe30fc4dca0fb5d6ecbbbfc4279d6b8
Clinton Herrick & Trevor Scherling <student@epicodus.com>
2015-09-15 16:11:15 -0700

    adds matinee spec and fuction

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-function Movie(movieTitle, movieRating) {
+function Movie(movieTitle, movieRating, moviePrice, movieTime) {
   this.movieTitle = movieTitle;
   this.movieRating = movieRating;
+  this.moviePrice = moviePrice;
+  this.movieTime = movieTime;
+
+  var ticketChild = { name: "child", price: 5 };
+  var ticketAdult = { name: "adult", price: 10 };
+  var ticketSenior = { name: "senior", price: 8 };
+
+  var moviePrice = { prices: [ticketChild, ticketAdult, ticketSenior] };
+  
+
 }
+
+
+Movie.prototype.matinee = function() {
+  return this.moviePrice - 2;
+}
+
+
+
+// function Ticket(ticketChild, ticketAdult, ticketSenior) {
+//   var ticketChild = { name: "child", price: 5 };
+//   var ticketAdult = { name: "adult", price: 10 };
+//   var ticketSenior = { name: "senior", price: 8 };
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,26 @@
 describe('Movie', function(){
   it("lists titles and ratings of movies", function(){
-    var testMovie = new Movie("ATOM", "R");
+    var testMovie = new Movie("ATOM", "R", 10, "6pm");
     expect(testMovie.movieTitle).to.equal("ATOM");
     expect(testMovie.movieRating).to.equal("R");
-    
+    expect(testMovie.moviePrice).to.equal(10);
+    expect(testMovie.movieTime).to.equal("6pm");
+
   });
+
+  it("creates matinee ticket prices", function() {
+    var testMovie = new Movie("ATOM", "R", 10, "6pm");
+    expect(testMovie.matinee()).to.equal(8);
+
+  });
+
 });
+
+//
+// describe('Ticket', function(){
+//   it("lists the price for the movies", function(){
+//    var testTicket = new Ticket({ticketChild: 5, ticketAdult: 10, ticketSenior: 8});
+//       expect(ticketMatinee.ticketChild).to.eql(price - 2);
+//       // expect(testTicket.ticketChild).to.equal(price - 2);
+//   });
+// });
